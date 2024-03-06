7b16a24d639f908b2afabdabb827720f6a4acefd
calvin abraham <calvin.abraham7@gmail.com>
2024-03-06 20:08:00 +0530

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -16,9 +16,9 @@ const prodConfig = {
     new ModuleFederationPlugin({
       name: 'myva_container',
       remotes: {
-        todo: `myva_todo@${domain}/myva_todo/latest/remoteEntry.js`,
-        cricket: `myva_cricket@${domain}/myva_cricket/latest/remoteEntry.js`,
-        auth: `myva_auth@${domain}/myva_auth/latest/remoteEntry.js`,
+        todo: `/myva_todo/latest/remoteEntry.js`,
+        cricket: `/myva_cricket/latest/remoteEntry.js`,
+        auth: `/myva_auth/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
