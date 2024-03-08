a40abc5c28b02fa65f9585ca57e351d77638526d
calvin.abraham7@gmail.com <calvin.abraham7@gmail.com>
2024-03-08 15:52:04 +0530

    added cdn config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,20 +4,21 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 const path = require('path');
 
-const domain = process.env.PRODUCTION_DOMAIN || 'http://myvas3bucket.s3-website.eu-north-1.amazonaws.com';
+const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    publicPath: '/myva_container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'myva_container',
       remotes: {
-        todo: `${domain}/myva_todo/latest/remoteEntry.js`,
-        cricket: `${domain}/myva_cricket/latest/remoteEntry.js`,
-        auth: `${domain}/myva_auth/latest/remoteEntry.js`,
+        todo: `marketing@${domain}/myva_todo/latest/remoteEntry.js`,
+        cricket: `marketing@${domain}/myva_cricket/latest/remoteEntry.js`,
+        auth: `marketing@${domain}/myva_auth/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
