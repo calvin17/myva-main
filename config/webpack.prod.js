988260d02bf54c17ed23b7028c68198e459cd391
calvin.abraham7@gmail.com <calvin.abraham7@gmail.com>
2024-03-08 14:25:17 +0530

    moving the deployment to aws

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,7 +4,7 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 const path = require('path');
 
-const domain = process.env.PRODUCTION_DOMAIN;
+const domain = process.env.PRODUCTION_DOMAIN || 'http://myvas3bucket.s3-website.eu-north-1.amazonaws.com';
 
 const prodConfig = {
   mode: 'production',
